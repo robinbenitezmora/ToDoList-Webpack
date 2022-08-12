8c41d3b72b6de7a9702efd5729f3622b5abbd599
robinbenitezmora <robinbenitez1990@gmail.com>
2022-08-12 17:49:58 -0500

    Adjust code lines in differents files agree with linters

--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
 import './index.css';
 
-const tasks = {
-  data: [
-    { description: 'Learn JavaScript', completed: false, index: 1 },
-    { description: 'Learn React', completed: false, index: 2 },
-    { description: 'Learn Vue', completed: false, index: 3 },
-    { description: 'Learn Angular', completed: false, index: 4 },
-  ],
+const info = {
+  data: [{ description: 'Javascript', completed: false, index: 1 },
+    { description: 'HTML', completed: false, index: 2 },
+    { description: 'CSS', completed: false, index: 3 },
+    { description: 'React', completed: false, index: 4 },
+    { description: 'Node', completed: false, index: 5 },
+    { description: 'MongoDB', completed: false, index: 6 },
+    { description: 'Express', completed: false, index: 7 }],
 };
 
-const toDoList = tasks.data;
-const showtoDoList = () => {
+const toDoList = info.data;
+const displayToDo = () => {
   const ListElement = document.getElementById('list');
   ListElement.innerHTML = '';
+
   toDoList.forEach((data) => {
-    const toDoListItem = document.createElement('li');
-    const checkboxItem = document.createElement('input');
-    checkboxItem.classList.add('checkInput');
-    checkboxItem.setAttribute('type', 'checkbox');
-    checkboxItem.setAttribute('name', 'checkbox');
-    checkboxItem.setAttribute('value', data.index);
+    const todoLiElement = document.createElement('li');
+
+    const todoCheckboxElement = document.createElement('input');
+    todoCheckboxElement.classList.add('checkInput');
+    todoCheckboxElement.setAttribute('type', 'checkbox');
+    todoCheckboxElement.setAttribute('name', 'checkbox');
+    todoCheckboxElement.setAttribute('value', data.index);
 
     if (data.completed) {
-      checkboxItem.checked = true;
+      todoCheckboxElement.checked = true;
     }
 
-    const toDoListDescriptionItem = document.createElement('p');
-    toDoListDescriptionItem.classList.add('label');
-    toDoListDescriptionItem.innerText = data.description;
+    const todoDescriptionElement = document.createElement('p');
+    todoDescriptionElement.classList.add('label');
+    todoDescriptionElement.innerText = data.description;
 
-    toDoListItem.appendChild(checkboxItem);
-    toDoListItem.appendChild(toDoListDescriptionItem);
-    ListElement.appendChild(toDoListItem);
+    todoLiElement.appendChild(todoCheckboxElement);
+    todoLiElement.appendChild(todoDescriptionElement);
+    ListElement.appendChild(todoLiElement);
   });
 };
 
 window.onload = () => {
-  showtoDoList();
+  displayToDo();
 };
 
 const list = document.getElementById('list');
@@ -48,7 +51,7 @@ const btnEnter = document.getElementById('enter');
 const addTask = (task) => {
   const element = `
   <li class='list-item' id='element'>          
-    <i class='far fa-square co' id='0' data-='finished'></i>
+    <i class='far fa-square co" id='0' data-='finished'></i>
     <p class='text'>${task}</p>
     <i class='fas fa-trash de' id='0' data-='eliminated'></i>
   </li>
