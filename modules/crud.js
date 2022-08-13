67108e4f6b9bc4013411e30d87a62853a4b3b710
robinbenitezmora <robinbenitez1990@gmail.com>
2022-08-13 11:25:15 -0500

    Add code lines in order to accomplish project's requirements

--- modules/crud.js
+++ modules/crud.js
@@ -24,6 +24,7 @@ const showtasksList = () => {
   const edittasksList = (todo) => {
     isModified = true;
     todoModified = todo;
+    const desc = document.getElementById('input');
     desc.value = todo.description;
     desc.focus();
   };
@@ -41,17 +42,24 @@ const showtasksList = () => {
     showtasksList();
   };
 
+  const activetasksList = (todo) => {
+    for (let i = 0; i < tasksList.length; i += 1) {
+      if (tasksList[i].index === todo.index) {
+        tasksList[i].completed = !tasksList[i].completed;
+      }
+    }
+    saveData();
+  }; 
+
   tasksList.forEach((data) => {
     const li = document.createElement('li');
     const checkbox = document.createElement('input');
+    li.setAttribute('draggable', 'true');
+    li.classList.add('list-group-item');
     checkbox.classList.add('checkInput');
     checkbox.setAttribute('type', 'checkbox');
     checkbox.setAttribute('name', 'checkbox');
     checkbox.setAttribute('value', data.index);
-
-    if (data.completed) {
-      checkbox.checked = true;
-    }
 
     const todoDescription = document.createElement('p');
     todoDescription.classList.add('label');
@@ -78,11 +86,8 @@ const showtasksList = () => {
 
     ul.appendChild(li);
 
-    removeButton.addEventListener('click', () => {
-      removetasksList(data.index);
-    });
-
     const actions = () => {
+      const desc = document.getElementById('input');
       if (!isModified) {
         removeButton.classList.toggle('hide');
         desc.value = null;
@@ -96,7 +101,23 @@ const showtasksList = () => {
       }
     };
 
+    if (data.completed) {
+      checkbox.checked = true;
+      todoDescription.classList.add('commuted');
+    }
+
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        todoDescription.classList.add('commuted');
+      } else todoDescription.classList.remove('commuted');
+      activetasksList(data);
+    });
+
     pointsBtn.addEventListener('click', () => {
+      actions();
+    });
+
+    removeButton.addEventListener('click', () => {
       actions();
     });
 
@@ -149,15 +170,16 @@ const saveEdit = () => {
 
 const getIsModified = () => isModified;
 
-window.onload = () => {
-  getData();
+const clearAll = () => {
+  tasksList = tasksList.filter((todo) => !todo.completed);
+  tasksList = tasksList.map((todo, index) => (
+    { completed: todo.completed, description: todo.description, index: index + 1 }));
+  isModified = false;
+  desc.value = null;
+  saveData();
   showtasksList();
 };
 
-desc.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
-    e.preventDefault();
-    if (!getIsModified()) addtasksList();
-    else saveEdit();
-  }
-});
+export {
+  getIsModified, getData, addtasksList, saveEdit, showtasksList, clearAll,
+};
